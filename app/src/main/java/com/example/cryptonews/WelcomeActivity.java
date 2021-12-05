31085f1f221216bd895b46b6c9a5772f72b52233
ludzenjr <71195626+ludzenjr@users.noreply.github.com>
2021-12-04 22:35:44 -0500

    Added the welcome screen

---
+++ app/src/main/java/com/example/cryptonews/WelcomeActivity.java
@@ -0,0 +1,54 @@
+package com.example.cryptonews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World! Test");
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("Crypto_News", "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("Crypto_News", "Failed to read value.", error.toException());
+            }
+        });
+    }
+
+    public void onButtonSignUpClicked(View view) {
+
+
+    }
+
+    public void onButtonSignInClicked(View view) {
+
+    }
+}
